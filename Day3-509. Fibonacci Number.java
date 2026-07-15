652b2af63a4aa228c814920c00c98298426bc417
vasmiya <vasmiya59@gmail.com>
2026-07-15 09:37:49 +0530

    Add Fibonacci number calculation method
    Implement Fibonacci number calculation using iteration.

---
+++ Day3-509. Fibonacci Number.java
@@ -0,0 +1,14 @@
+   if(n<=1) return n;
+        int a=0;
+        int b=1;
+        for(int i=2;i<=n;i++){
+            int next=a+b;
+            a=b;
+            b=neclass Solution {
+    public int fib(int n) {
+     xt;
+        }
+        return b;
+          
+    }
+}
