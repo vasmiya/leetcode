ad66ef8a4209c5a4b74495c8cb9aed8a87c037f5
vasmiya <vasmiya59@gmail.com>
2026-07-15 11:05:17 +0530

    Implement palindrome check for integers

---
+++ Day3-palindrome number.java
@@ -0,0 +1,23 @@
+
+        class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if (x < 0){
+            return false;
+        }
+
+        int original = x;
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x = x / 10;
+            reversed = reversed * 10 + digit;
+        }
+
+        return original == reversed;
+    }
+        }
+
+        
+    
