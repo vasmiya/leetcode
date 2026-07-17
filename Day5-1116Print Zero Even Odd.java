60aa15939427c34bd5270adc08157853110beeba
vasmiya <vasmiya59@gmail.com>
2026-07-17 11:38:37 +0530

    Create Day5-1116Print Zero Even Odd.java

---
+++ Day5-1116Print Zero Even Odd.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
+
+class ZeroEvenOdd {
+    private int n;
+    private Semaphore zero = new Semaphore(1);
+    private Semaphore odd = new Semaphore(0);
+    private Semaphore even = new Semaphore(0);
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            zero.acquire();
+            printNumber.accept(0);
+            if (i % 2 == 0) {
+                even.release();
+            } else {
+                odd.release();
+            }
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            even.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            odd.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
+    }
+}
