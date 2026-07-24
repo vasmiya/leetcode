2c1ed2f62661de53db4baf0f712dd792b51439cb
vasmiya <vasmiya59@gmail.com>
2026-07-24 08:42:15 +0530

    Create Day10-10 Regular Expression Matching.java

---
+++ Day10-10 Regular Expression Matching.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        // Handle patterns like a*, a*b*, a*b*c* etc for empty string
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char pc = p.charAt(j - 1);
+                
+                if (pc == '.' || pc == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc == '*') {
+                    // Zero occurrence of preceding element
+                    dp[i][j] = dp[i][j - 2];
+                    
+                    // One or more occurrence, if char matches
+                    char prevChar = p.charAt(j - 2);
+                    if (prevChar == '.' || prevChar == s.charAt(i - 1)) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
